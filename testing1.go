64dd5b481a21684bae637c05c833b7f2c65c886f
Bauti032 <128093547+Bauti032@users.noreply.github.com>
2025-10-06 23:51:38 +0000

    ultima teoria

---
+++ testing1.go
@@ -0,0 +1,18 @@
+package main
+
+
+func main() {
+    var numbers, sequence, replacement []int
+    //desarrollar item 2
+}  
+
+
+func Reemplaza(numbers, sequence, replacement []int) []int {
+    //desarrollar item 1.a
+	return numbers
+}
+
+
+func ContarElementos(numbers []int) []int {
+    //desarrollar item 1.b
+}
